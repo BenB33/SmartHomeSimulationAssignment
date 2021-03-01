e36130647eb6e600e0f97f92df81aee9fbf522bc
BenB33 <benharrison9898@gmail.com>
2021-03-01 12:10:10 +0000

    Deleted old files

--- Choice.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum class MainMenuChoice { Login, Device_Menu, Sensor_Menu, Exit };
-
-enum class DeviceMenuChoice { View_Device_Status, Change_Device_Status, Configure_Device_Intensity, Back };
-
-enum class SensorMenuChoice { View_Sensor_List, Read_Sensor_Data, Back };
--- Validation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class Validation
-{
-public:
-	static uint16_t integerValidation(uint16_t limit);
-	std::string stringValidation();
-};
